02ac6abe4da9e33adea96ecb07c0dcf4ddf1064d
iuliavisan <visaniuliaraluca@gmail.com>
2026-03-31 19:03:14 +0300

    work on Pierdere.cs

--- EcoChef.Web/Models/Pierdere.cs
+++ EcoChef.Web/Models/Pierdere.cs
@@ -1,15 +1,30 @@
-﻿namespace EcoChef.Web.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EcoChef.Web.Models
 {
     public class Pierdere
     {
         public int Id { get; set; }
 
-        public string NumePierdere { get; set; }
+        [Required(ErrorMessage = "Numele produsului este obligatoriu!")]
+        [Display(Name = "Numele produsului de scăzut din stoc")]
+
+        public int IngredientId { get; set; }
+        public Ingredient? Ingredient { get; set; }
+
+        [Required(ErrorMessage = "Cantitatea pierderii este obligatorie!")]
+        [Display(Name = "Cantitate:")]
         public decimal CantitatePierdere { get; set; }
 
+        [Required(ErrorMessage ="Motivul pierderii este necesar!")]
+        [Display(Name = "Motivul pierderii:")]
         public string MotivPierdere { get; set; }
 
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Data pierderii")]
         public DateTime DataPierdere{ get; set; }
+
 
         public decimal PretPierdere { get; set; }
     }
