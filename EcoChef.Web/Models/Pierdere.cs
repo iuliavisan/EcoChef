656be26c84940ab02b138a8a3e5cb601146364bc
iuliavisan <visaniuliaraluca@gmail.com>
2026-03-31 17:41:57 +0300

    -creeaza clasa Pierdere

---
+++ EcoChef.Web/Models/Pierdere.cs
@@ -0,0 +1,16 @@
+﻿namespace EcoChef.Web.Models
+{
+    public class Pierdere
+    {
+        public int Id { get; set; }
+
+        public string NumePierdere { get; set; }
+        public decimal CantitatePierdere { get; set; }
+
+        public string MotivPierdere { get; set; }
+
+        public DateTime DataPierdere{ get; set; }
+
+        public decimal PretPierdere { get; set; }
+    }
+}
