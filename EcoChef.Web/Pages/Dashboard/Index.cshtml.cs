66b26ca8ff709d732d02bab3b9a20dd46a8ba7ab
iuliavisan <visaniuliaraluca@gmail.com>
2026-04-08 11:50:11 +0300

    Dashboard begin

---
+++ EcoChef.Web/Pages/Dashboard/Index.cshtml.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EcoChef.Web.Data;
+using EcoChef.Web.Models;
+
+
+namespace EcoChef.Web.Pages.Dashboard
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public List <Ingredient> IngredienteExpira {  get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var limita = DateTime.Now.AddDays(3);
+            IngredienteExpira = await _context.Ingrediente
+                .Where(i => i.DataExpirare <= limita)
+                .ToListAsync();
+        }
+    }
+}
