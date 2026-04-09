ffc56c8828a19b859a590a608cc9da54f0965b72
iuliavisan <visaniuliaraluca@gmail.com>
2026-04-09 21:38:44 +0300

    grafice-in progress

---
+++ EcoChef.Web/Pages/Reports/Index.cshtml.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using EcoChef.Web.Data;
+using EcoChef.Web.Models;
+using System.Text.Json;
+
+namespace EcoChef.Web.Pages.Reports
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+        public IndexModel(ApplicationDbContext context)
+        {
+            _context = context; 
+        }
+
+        public string ReteteJson { get; set; } = "[]";
+        public string PierderiJson {  get; set; }= "[]";
+        public decimal TotalProfit { get; set; }
+        public decimal TotalPierderi { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            var primeLuna = new DateTime(DateTime.Now.Year, DateTime.Now.Month, 1);
+
+            //cele mai gatite retete
+            var reteteGatite = await _context.IstoricGatire
+                .Where(g => g.DataGatirii >= primeLuna)
+                .Include(g => g.Reteta)
+                .GroupBy(g => g.Reteta.Nume)
+                .Select(g => new
+                {
+                    Nume = g.Key,
+                    TotalPortii = g.Sum(x => x.NrPortii)
+                })
+                .OrderByDescending(g => g.TotalPortii)
+                .ToListAsync();
+
+            //cele mai mari pierderi
+
+            var pierderi = await _context.Pierderi
+                .Where(p => p.DataPierdere >= primeLuna)
+                .Include(p => p.Ingredient)
+                .GroupBy(p => p.Ingredient.Nume)
+                .Select(g => new
+                {
+                    Nume = g.Key,
+                    TotalPierdere = g.Sum(x => x.PretPierdere)
+                })
+                .OrderByDescending(g => g.TotalPierdere)
+                .ToListAsync();
+
+            //totaluri
+            var istoricLuna = await _context.IstoricGatire
+                .Where(g => g.DataGatirii >= primeLuna)
+                .ToListAsync();
+
+            TotalProfit = istoricLuna.Sum(g => g.PretVanzareTotal - g.CostTotal);
+            TotalPierderi = pierderi.Sum(p => p.TotalPierdere);
+
+            //serializare pt js
+            ReteteJson = JsonSerializer.Serialize(reteteGatite);
+            PierderiJson = JsonSerializer.Serialize(pierderi);
+        }
+    }
+}
